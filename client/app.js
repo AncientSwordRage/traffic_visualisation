80865125e1b2a52d89b5a627ba20a4adaace4aa8
AncientSwordRage <pureferret@gmail.com>
2017-07-01 18:11:18 +0100

    Added traffic data and app shell

--- client/app.js
+++ client/app.js
@@ -2,9 +2,21 @@
 
 //* App Module 
 
-angular.module('app', [
-])
+angular.module('app', [])
+    .controller('appController', function() {
+        /* Set up*/
+        var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-.controller('appController', function(){
-});
+        var format = d3.format(",d");
 
+        var treemap = d3.treemap()
+            .size([width, height])
+            .round(true)
+            .padding(1);
+
+        /* Data */
+
+        /* Create Tree */
+        /* Draw Tree */
+        /* Draw other pages */
+    });
